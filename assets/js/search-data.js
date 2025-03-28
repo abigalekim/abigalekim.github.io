e10bb554515e07fa87ad38f8d8dc1c716df9b430
abigalekim <abigalekim0417@gmail.com>
2025-03-28 03:53:23 +0000

    Deploying to gh-pages from @ abigalekim/abigalekim.github.io@251c83fe91a7a77874ccc680207a86b490112658 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/courses/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-subset-of-database-systems-and-why-they-39-re-interesting-part-1",
         
           title: "A Subset of Database Systems (and Why They&#39;re Interesting), Part 1",
