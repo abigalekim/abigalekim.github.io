cedee9d04c167f81379f8f64f685f5b117abfd08
abigalekim <abigalekim0417@gmail.com>
2025-08-26 17:43:02 +0000

    Deploying to gh-pages from @ abigalekim/abigalekim.github.io@ec7112e6421d16dc4e3f8950f367be227da4cf51 🚀

---
+++ assets/js/giscus-setup.js
@@ -0,0 +1,42 @@
+function determineGiscusTheme() {
+  
+    let theme =
+      localStorage.getItem("theme") ||
+      document.documentElement.getAttribute("data-theme") ||
+      "system";
+
+    if (theme === "dark") return "";
+    if (theme === "light") return "";
+
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return prefersDark ? "" : "";
+  
+}
+
+(function setupGiscus() {
+  let giscusTheme = determineGiscusTheme();
+
+  let giscusAttributes = {
+    src: "https://giscus.app/client.js",
+    "data-repo": "abigalekim/abigalekim.github.io",
+    "data-repo-id": "",
+    "data-category": "Comments",
+    "data-category-id": "",
+    "data-mapping": "title",
+    "data-strict": "1",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "bottom",
+    "data-theme": giscusTheme,
+    "data-lang": "en",
+    crossorigin: "anonymous",
+    async: true,
+  };
+
+  let giscusScript = document.createElement("script");
+  Object.entries(giscusAttributes).forEach(([key, value]) =>
+    giscusScript.setAttribute(key, value)
+  );
+  document.getElementById("giscus_thread").appendChild(giscusScript);
+})();
+
